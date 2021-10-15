8550f6ad7b375b5b94b8bdb98ab2ef20a34c647a
Marcela Ribeiro <marcelaribeiro.ti@gmail.com>
2021-10-15 09:25:07 +0200

    test sum method

--- src/test/java/com/test/calculator/CalculatorApplicationTest.java
+++ src/test/java/com/test/calculator/CalculatorApplicationTest.java
@@ -1,13 +1,66 @@
 package com.test.calculator;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-class CalculatorApplicationTests {
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.test.calculator.controller.CalculatorController;
+import com.test.calculator.service.CalculatorService;
+import com.test.calculator.service.impl.CalculatorServiceImpl;
+
+@WebMvcTest(CalculatorController.class)
+public class CalculatorApplicationTest {
+
+	
+	@MockBean
+	private CalculatorService calculatorService;
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private CalculatorServiceImpl calculatorServiceImpl = new CalculatorServiceImpl();
 
 	@Test
-	void contextLoads() {
+	public void testSum_OK() throws Exception {
+		
+		calculatorService = new CalculatorServiceImpl();
+
+		Long number1 = Long.valueOf(2);
+		Long number2 = Long.valueOf(3);
+
+		assertTrue(calculatorService.sum(number1, number2) == 5, () -> "Sum should be equal to 5");
+
+	}
+
+	@Test
+	public void testSumUrl_OK() throws Exception {
+		Long number1 = Long.valueOf(1);
+		Long number2 = Long.valueOf(2);
+		Long result = Long.valueOf(3);
+
+		Mockito.when(calculatorService.sum(number1, number2)).thenReturn(result);
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/calculator/sum/1/2").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk()).andExpect(content().string(containsString("Sum Result")));
+
+	}
+
+	@Test
+	public void testSum_KO_400() throws Exception {
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/calculator/sum/a/b").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest()).andExpect(content().string(containsString("Message")));
+
 	}
 
 }
